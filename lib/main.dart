1a2d9c64a632d8888b0cfe228cf4021374e5f28a
Alex Tsirozidis <altsirozidis@gmail.com>
2023-02-20 13:43:16 +0200

    arxh kataskeyis allws selidwn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mocks/presentation/pages/home/home_page.dart';
+import 'package:mocks/presentation/pages/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: HomeScreen(),
     );
   }
 }
