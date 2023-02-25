163e1ab5a9b229d509ae2ec49ef1614f990fdf4c
Giorgos Angelopoulos <agelogeo@gmail.com>
2023-02-25 21:51:40 +0200

    Destination Carousel completed

--- lib/presentation/widgets/destination_carousel.dart
+++ lib/presentation/widgets/destination_carousel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mocks/models/destination_model.dart';
 
 class DestinationCarousel extends StatelessWidget {
@@ -9,46 +10,200 @@ class DestinationCarousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                "Top Destinations",
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.5),
+      children: const [
+        TopRowWithSeeAll(),
+        ListCarousel(),
+      ],
+    );
+  }
+}
+
+class ListCarousel extends StatelessWidget {
+  const ListCarousel({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          Destination destination = destinations[index];
+          return Container(
+            margin: const EdgeInsets.all(10),
+            width: 210,
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                WhiteContainerWithText(destination: destination),
+                ImageSection(destination: destination),
+              ],
+            ),
+          );
+        },
+        itemCount: destinations.length,
+      ),
+    );
+  }
+}
+
+class TopRowWithSeeAll extends StatelessWidget {
+  const TopRowWithSeeAll({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Top Destinations",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.5,
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all(
+                Theme.of(context).primaryColor,
               ),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all(Theme.of(context).primaryColor),
-                  textStyle: MaterialStateProperty.all(
-                    const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 1.0),
-                  ),
+              textStyle: MaterialStateProperty.all(
+                const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.0),
+              ),
+            ),
+            child: const Text("See All"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WhiteContainerWithText extends StatelessWidget {
+  const WhiteContainerWithText({
+    super.key,
+    required this.destination,
+  });
+
+  final Destination destination;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 15,
+      child: Container(
+        height: 120,
+        width: 200,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${destination.activities.length} activities",
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.2,
                 ),
-                child: const Text("See All"),
+              ),
+              Text(
+                destination.description,
+                style: const TextStyle(color: Colors.grey),
               ),
             ],
           ),
         ),
-        Container(
-            height: 300,
-            color: Colors.blue,
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Text(destinations[index].city);
-              },
-              itemCount: destinations.length,
-            ))
-      ],
+      ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({
+    super.key,
+    required this.destination,
+  });
+
+  final Destination destination;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 2),
+            blurRadius: 6,
+          )
+        ],
+      ),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image(
+              height: 180,
+              width: 180,
+              image: AssetImage(destination.imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            left: 10,
+            bottom: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  destination.city,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      FontAwesomeIcons.locationArrow,
+                      size: 10,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      destination.country,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
