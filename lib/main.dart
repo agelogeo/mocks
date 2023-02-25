b66e99cb99628d11392f7b4f1efff9ff6f12accd
Alex Tsirozidis <altsirozidis@gmail.com>
2023-02-25 18:27:59 +0200

    main done

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Travel UI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
       ),
       home: HomeScreen(),
     );
